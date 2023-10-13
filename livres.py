dcac088955b0f89a17b1bc0d77e6e8e1fd8683a3
ThalyaC1 <nathalie.mamain@yahoo.fr>
2023-10-13 23:06:16 +0200

    insertion def image(url)

--- livres.py
+++ livres.py
@@ -5,10 +5,11 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from urllib.request import urlretrieve
 
 
 # indiquer la page internet consultée et accéder à son code html source
-#url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 #url = ph0.url
 
 def analyse_page_livre(url):
@@ -219,4 +220,53 @@ livre1(url,fichier)
 url="http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
 livre_suivant(url,fichier)
 """
+def image(url): #ok 13/10/23
+    liste=valeurs_tableau(url)
+    lien_image0=liste[9]
+    #exple : http://books.toscrape.com/../../media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg
+    lien_image=lien_image0.replace("../../", "")
+    print("lien de l'image: ",lien_image)
+
+    titre_image0=liste[2]
+    titre_image1=titre_image0.replace(" ","_")
+    titre_image2=titre_image1.split("_")
+    titre_image3 = list(filter(None, titre_image2))
+    titre_image4='_'.join(titre_image3)
+    titre_image=titre_image4
+    print("titre de l'image souhaité: ",titre_image)
+
+
+    def nom_image_brut(lien_image):
+        lien1=lien_image.split('/')
+        nom_image_base=lien1[-1]
+        return (nom_image_base)
+
+    nom_image_base=nom_image_brut(lien_image)
+    print("nom de l'image sur le site: ",nom_image_base)
+
+    dossier0=liste[7]
+    dossier1=dossier0.lower()
+    dossier2=dossier1.replace(" ","_")
+    dossier=dossier2 + "/"
+
+    def download_image(lien_image, dossier, titre_image):
+        full_path = dossier + titre_image+".jpg"
+        urlretrieve(lien_image, full_path)
+
+    download_image(lien_image, dossier, titre_image)
+    return()
+
+
+"""
+url = "http://books.toscrape.com/catalogue/soumission_998/index.html"
+fichier='livre.csv'
+
+livre1(url,fichier)
+
+url="http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
+livre_suivant(url,fichier)
+
+image(url)
+"""
+
 
