2de2ca4cf76b61769223a80757e8b3ac7465f2da
ThalyaC1 <nathalie.mamain@yahoo.fr>
2023-10-13 03:26:31 +0200

    création de dossiers, simplification du code, suppression des fichiers .txt, supression des requests superflus, réécriture de certains passages. problème mettre les fichiers dans les dossiers.

--- site_books.py
+++ site_books.py
@@ -2,23 +2,23 @@
 Projet 2 Phase 3
 1. Interroger le site "Books to scrappe"; 
 2. Récupérer toutes les catégories ;
-3. Créer un fichier csv contenant tous les livres.
+3. Demande du 6/10/2023 : creation pour chaque catégorie d'un dossier devant accueillir en phase 4, les images
+4. Créer un fichier csv contenant tous les livres par catégorie dans le dossier qui lui est attittré.
 """
 
 
 import requests
 from bs4 import BeautifulSoup
-import category
-
+from category import creation_fichiers_csv
+from os import mkdir
 
 #1. Interroger le site "Books to scrappe"
 
 url="http://books.toscrape.com/"
 
-#2. Récupérer toutes les catégories (nouvelle définition puisque 
-# "infex.html" ne figure pas dans le menu home" - 
-# !!! Question : books1 présente l'ensemble des livres, doit-il figurer dans les catégories, 
-# parti pris: non)
+#2. Récupérer toutes les catégories (nouvelle définition de "category" par rapport au fichier category puisque 
+# "infex.html" ne figure pas dans le menu "home" - 
+# sortir "books" qui n'est pas une catégorie, mais l'ensemble des livres du site.
 
 def base_category1(url):                              #ok, 05/10/2023
     page = requests.get(url)
@@ -33,100 +33,65 @@ def base_category1(url):                              #ok, 05/10/2023
     #print(liste_category0)
     return(liste_category0)
 
-def category1(url):                                    #ok, 05/10/2023
-    liste_category0=base_category1(url)
+def category1(url):                                  #ok, 05/10/2023, modifié le 13/10/23
     liste_category2=[]
-    for lien in liste_category0:
+    for lien in base_category1(url):
         lien1=lien.split('/')
         lien1.pop()
         nom_category=lien1[3]
         liste_category2.append(nom_category)
         #exple : nom_category='science-fiction_16'
-    #print("liste des catégories",len(liste_category), liste_category)
+    #print("liste des catégories",len(liste_category2), liste_category2)
     return(liste_category2)
 
 def adresse_category(url):                              #ok, 05/10/2023
-    liste_category0=base_category1(url)
     liste_adresse_category=[]
-    for lien_partiel in liste_category0:
+    for lien_partiel in base_category1(url):
         adresse_category=url+lien_partiel
         liste_adresse_category.append(adresse_category)
         # exple : adresse_category='http://books.toscrape.com/catalogue/category/books/romance_8/index.html'
-    #print(liste_adresse_category)
+    print(liste_adresse_category)
     return(liste_adresse_category)
 
 
-#3. Créer un fichier csv contenant tous les livres.
+#3. & 4. Créer un fichier csv par catégorie dans un dossier contenant tous les livres.
 
-def creation_noms_fichiers_text(url): #ok,05/10/2023
-    liste_noms_fichiers_txt=[]
+def creation_noms_dossiers(url): #ok 12/10/13
+    liste_noms_dossiers=[]
     liste_category=category1(url)  
     for nom_category in liste_category:
-        nom_category_text="p2_ph3_"+nom_category+".text"
-        liste_noms_fichiers_txt.append(nom_category_text)
-    #print(liste_noms_fichiers_txt,len(liste_noms_fichiers_txt))
-    return(liste_noms_fichiers_txt) 
+        nom_category_dossier=nom_category
+        liste_noms_dossiers.append(nom_category_dossier)
+    #print(liste_noms_dossiers,len(liste_noms_dossiers))
+    return(liste_noms_dossiers)
 
-def creation_noms_fichiers_csv(url): #ok, 05/10/2023
+def creation_dossiers(url): # ok, 13/10/23
+    for dossier in creation_noms_dossiers(url):
+        try:
+            mkdir(dossier)
+        except OSError as e:
+            pass
+        finally :
+            pass
+    return()
+
+def creation_noms_fichiers_csv(url): #ok, modifié le 13//10/2023
     liste_noms_fichiers_csv=[]
     liste_category=category1(url)  
     for nom_category in liste_category:
-        nom_category_csv="p2_ph3_"+nom_category+".csv"
+        nom_category_csv=nom_category+".csv"
         liste_noms_fichiers_csv.append(nom_category_csv)
-    #print(liste_noms_fichiers_csv,len(liste_noms_fichiers_csv))
+    print(liste_noms_fichiers_csv,len(liste_noms_fichiers_csv))
     return(liste_noms_fichiers_csv) 
-  
-def creation_fichiers_par_categorie(url):
-    liste_fichiers_txt=creation_noms_fichiers_text(url)
-    liste_fichiers_csv=creation_noms_fichiers_csv(url)
-    liste_categories=adresse_category(url)
-    for lien in liste_categories:
-        for fichier1_text in liste_fichiers_txt:
-            for fichier2_csv in liste_fichiers_csv:
-                ph2.creation_fichiers(lien, fichier1_text,fichier2_csv)
 
-
-#creation_fichiers_par_categorie(url)
-liste_fichiers_txt=creation_noms_fichiers_text(url)
+#creation_fichiers_par_categorie(url) #modifié le 13/10/23
 liste_fichiers_csv=creation_noms_fichiers_csv(url)
 liste_categories=adresse_category(url)
-liste_parametre = list(zip(liste_categories, liste_fichiers_txt,liste_fichiers_csv))
-#exple : liste_parametre[0]='http://books.toscrape.com/catalogue/category/books/travel_2/index.html', 'p2_ph3_travel_2.text', 'p2_ph3_travel_2.cvs'
-#print(liste_parametre[0])
+liste_parametre = list(zip(liste_categories, liste_fichiers_csv))
+#print(liste_parametre[0]) exple : ('http://books.toscrape.com/catalogue/category/books/travel_2/index.html', 'travel_2.csv')
 
 for paramet in liste_parametre:
     url1=str(paramet[0])
-    fichier1_txt=str(paramet[1])
-    fichier2_csv=str(paramet[2])
-    print(paramet,"\n", "1.",url1,"\n","2.",fichier1_txt,"\n","3.",fichier2_csv)
-    ph2.creation_fichiers(url1,fichier1_txt,fichier2_csv)
-    #time.sleep(1)
-
-"""
-def creation_fichiers_par_cat(url,fichier1_txt,fichier2_csv):
-    for adresse in toutes_les_pages(url):
-        url=adresse
-        page = requests.get(url) 
-        if page.ok:
-            BeautifulSoup(page.content, 'html.parser')
-            livres=liste_livres(url)
-            print(adresse,len(livres)) #ok
-            #enregistrement d'un fichier txt contenant tous les liens des livres d'une catégorie
-            with open (fichier1_txt,'a') as cat5: #ok
-                for livre in livres:
-                    cat5.write(livre+'\n')
-
-    url_livre1=liste_livres(url)[0]
-    ph1.en_tete_csv(url_livre1,fichier2_csv)
-        
-    with open (fichier1_txt,'r') as cat5:
-        for ligne in cat5:
-            url=ligne.strip()
-            response=requests.get(url)
-            if response.ok:
-                valeurs_client=ph1.valeurs_tableau(url)
-                with open(fichier2_csv,'a') as fich_cat5:
-                    ligne = valeurs_client
-                    writer = csv.writer(fich_cat5, delimiter=',')
-                    writer.writerow(ligne)
-"""
+    fichier1_csv=str(paramet[1])
+    print(paramet,"\n", "1.",url1,"\n","2.",fichier1_csv)
+    creation_fichiers_csv(url1,fichier1_csv)
